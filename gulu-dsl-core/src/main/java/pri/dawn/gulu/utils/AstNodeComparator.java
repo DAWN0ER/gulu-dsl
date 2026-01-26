7396b752ce1ae1d3077123337cf7b5ecd0ba44b6
Dawn Yang <dawn112358@outlook.com>
2026-01-27 00:46:00 +0800

    feat(utils): 添加AST节点比较器和节点值工具类
    - 新增AstNodeComparator实现GuluAstNode比较功能
    - 创建GuluNodeValueUtils用于获取节点上下文值和转换规则
    - 提供基础的节点比较框架支持后续完善
    - 预留节点值处理的核心方法接口

---
+++ gulu-dsl-core/src/main/java/pri/dawn/gulu/utils/AstNodeComparator.java
@@ -0,0 +1,20 @@
+package pri.dawn.gulu.utils;
+
+import pri.dawn.gulu.ast.GuluAstNode;
+
+import java.util.Comparator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author Dawn Yang
+ * @since 2026/01/27/0:41
+ */
+public class AstNodeComparator implements Comparator<GuluAstNode> {
+    @Override
+    public int compare(GuluAstNode o1, GuluAstNode o2) {
+        // 完善工具类，比较相关的只用这个作比较
+        return 0;
+    }
+}
---
+++ gulu-dsl-core/src/main/java/pri/dawn/gulu/utils/GuluNodeValueUtils.java
@@ -0,0 +1,13 @@
+package pri.dawn.gulu.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author Dawn Yang
+ * @since 2026/01/27/0:44
+ */
+public class GuluNodeValueUtils {
+
+    // TODO 获取节点在当前上下文的 value 值和对应的转换规则
+}
